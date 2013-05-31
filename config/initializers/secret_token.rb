952526a12be4b9d7084887fbb6c2bd75954b3fef
Shiri Avnery <shiri@wegofair.com>
2013-05-30 17:10:29 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'c5e172439ace085dbe817522c96ce6810c3eec261246fdec37eb3eec8c5e23081d301703b46b56f7086a361c177ede6918b96356c1f468a6a3eb19b4b97a7a25'
